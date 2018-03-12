cae4dd553e20823b1512f0dcce8b4d28a50524f9
henryolik <jindrarouball@seznam.cz>
2018-03-12 19:51:38 +0100

    Added WUreset

--- updater.Designer.vb
+++ updater.Designer.vb
@@ -23,45 +23,45 @@ Partial Class updater
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(updater))
-        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
+        Me.pb_load = New System.Windows.Forms.ProgressBar()
+        Me.la_perc = New System.Windows.Forms.Label()
+        Me.la_text = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'ProgressBar1
+        'pb_load
         '
-        Me.ProgressBar1.Location = New System.Drawing.Point(12, 77)
-        Me.ProgressBar1.Name = "ProgressBar1"
-        Me.ProgressBar1.Size = New System.Drawing.Size(372, 29)
-        Me.ProgressBar1.TabIndex = 0
+        Me.pb_load.Location = New System.Drawing.Point(12, 77)
+        Me.pb_load.Name = "pb_load"
+        Me.pb_load.Size = New System.Drawing.Size(372, 29)
+        Me.pb_load.TabIndex = 0
         '
-        'Label1
+        'la_perc
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 61)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(10, 13)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "."
+        Me.la_perc.AutoSize = True
+        Me.la_perc.Location = New System.Drawing.Point(12, 61)
+        Me.la_perc.Name = "la_perc"
+        Me.la_perc.Size = New System.Drawing.Size(10, 13)
+        Me.la_perc.TabIndex = 1
+        Me.la_perc.Text = "."
         '
-        'Label2
+        'la_text
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
-        Me.Label2.Location = New System.Drawing.Point(25, 9)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(350, 39)
-        Me.Label2.TabIndex = 2
-        Me.Label2.Text = "Probíhá aktualizace..."
+        Me.la_text.AutoSize = True
+        Me.la_text.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.la_text.Location = New System.Drawing.Point(25, 9)
+        Me.la_text.Name = "la_text"
+        Me.la_text.Size = New System.Drawing.Size(350, 39)
+        Me.la_text.TabIndex = 2
+        Me.la_text.Text = "Probíhá aktualizace..."
         '
         'updater
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(397, 126)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.la_text)
+        Me.Controls.Add(Me.la_perc)
+        Me.Controls.Add(Me.pb_load)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "updater"
         Me.Text = "Míní Studios Updater"
@@ -69,8 +69,8 @@ Partial Class updater
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
-    Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents pb_load As System.Windows.Forms.ProgressBar
+    Friend WithEvents la_perc As System.Windows.Forms.Label
+    Friend WithEvents la_text As System.Windows.Forms.Label
 End Class
 
--- updater.vb
+++ updater.vb
@@ -24,7 +24,10 @@ Public Class updater
         If My.Application.CommandLineArgs.Contains("-bf2") = True Then
             bf2()
         End If
-        If My.Application.CommandLineArgs.Contains("-elder") = False And My.Application.CommandLineArgs.Contains("-elderbeta") = False And My.Application.CommandLineArgs.Contains("-launcher") = False And My.Application.CommandLineArgs.Contains("-mhd") = False And My.Application.CommandLineArgs.Contains("-antiezo") = False And My.Application.CommandLineArgs.Contains("-fdc") = False And My.Application.CommandLineArgs.Contains("-bf2") = False Then
+        If My.Application.CommandLineArgs.Contains("-wur") = True Then
+            wur()
+        End If
+        If My.Application.CommandLineArgs.Contains("-elder") = False And My.Application.CommandLineArgs.Contains("-elderbeta") = False And My.Application.CommandLineArgs.Contains("-launcher") = False And My.Application.CommandLineArgs.Contains("-mhd") = False And My.Application.CommandLineArgs.Contains("-antiezo") = False And My.Application.CommandLineArgs.Contains("-fdc") = False And My.Application.CommandLineArgs.Contains("-bf2") = False And My.Application.CommandLineArgs.Contains("-wur") = False Then
             MsgBox("No argument was found. / Nebyl zadán nebo rozpoznán žádný argument.", MsgBoxStyle.Critical, "Error")
             Application.Exit()
         End If
@@ -41,59 +44,59 @@ Public Class updater
             Snooze(1)
         Loop
         Me.Show()
-        ProgressBar1.Minimum = 0
-        ProgressBar1.Maximum = 100
-        ProgressBar1.Value = 1
+        pb_load.Minimum = 0
+        pb_load.Maximum = 100
+        pb_load.Value = 1
         Try
             wc.DownloadFile("https://dl.ministudios.ml/mini/launcher/files/launcher.exe", Application.StartupPath & "\launcher.exe")
-            ProgressBar1.Value = 10
+            pb_load.Value = 10
             wc.DownloadFile("https://dl.ministudios.ml/mini/launcher/files/mini.exe", Application.StartupPath & "\mini.exe")
-            ProgressBar1.Value = 20
+            pb_load.Value = 20
             wc.DownloadFile("https://dl.ministudios.ml/mini/launcher/files/mini2.exe", Application.StartupPath & "\mini2.exe")
-            ProgressBar1.Value = 30
+            pb_load.Value = 30
             wc.DownloadFile("https://dl.ministudios.ml/mini/launcher/files/icon.ico", Application.StartupPath & "\icon.ico")
-            ProgressBar1.Value = 35
+            pb_load.Value = 35
         Catch ex As Exception
             MsgBox("Aktualizace se nezdařila! Chyba: " & ex.ToString, MsgBoxStyle.Critical, "Error")
             Application.Exit()
         End Try
         If My.Computer.FileSystem.DirectoryExists(Application.StartupPath & "/7z") Then
             System.IO.Directory.Delete(Application.StartupPath & "/7z", True)
-            ProgressBar1.Value = 40
+            pb_load.Value = 40
         Else
-            ProgressBar1.Value = 40
+            pb_load.Value = 40
         End If
         If My.Computer.FileSystem.FileExists(Application.StartupPath & "/7za.exe") Then
             My.Computer.FileSystem.DeleteFile(Application.StartupPath & "/7za.exe")
-            ProgressBar1.Value = 50
+            pb_load.Value = 50
         Else
-            ProgressBar1.Value = 50
+            pb_load.Value = 50
         End If
         If My.Computer.FileSystem.FileExists(Application.StartupPath & "/7za.dll") Then
             My.Computer.FileSystem.DeleteFile(Application.StartupPath & "/7za.dll")
-            ProgressBar1.Value = 60
+            pb_load.Value = 60
         Else
-            ProgressBar1.Value = 60
+            pb_load.Value = 60
         End If
         If My.Computer.FileSystem.FileExists(Application.StartupPath & "/7zxa.dll") Then
             My.Computer.FileSystem.DeleteFile(Application.StartupPath & "/7zxa.dll")
-            ProgressBar1.Value = 70
+            pb_load.Value = 70
         Else
-            ProgressBar1.Value = 70
+            pb_load.Value = 70
         End If
         If My.Computer.FileSystem.FileExists(Application.StartupPath & "/update.bat") Then
             My.Computer.FileSystem.DeleteFile(Application.StartupPath & "/update.bat")
-            ProgressBar1.Value = 80
+            pb_load.Value = 80
         Else
-            ProgressBar1.Value = 80
+            pb_load.Value = 80
         End If
         If My.Computer.FileSystem.FileExists(Application.StartupPath & "/mini.zip") Then
             My.Computer.FileSystem.DeleteFile(Application.StartupPath & "/mini.zip")
-            ProgressBar1.Value = 90
+            pb_load.Value = 90
         Else
-            ProgressBar1.Value = 90
+            pb_load.Value = 90
         End If
-        ProgressBar1.Value = 100
+        pb_load.Value = 100
         Process.Start(Application.StartupPath & "\launcher.exe")
         Me.Close()
     End Sub
@@ -109,23 +112,23 @@ Public Class updater
             Snooze(1)
         Loop
         Me.Show()
-        ProgressBar1.Minimum = 0
-        ProgressBar1.Maximum = 100
+        pb_load.Minimum = 0
+        pb_load.Maximum = 100
         Try
-            ProgressBar1.Value = 1
+            pb_load.Value = 1
             wc.DownloadFile("http://nonssl.dl.ministudios.ml/mini/elder/files/launcher.exe", Application.StartupPath & "\launcher.exe")
-            ProgressBar1.Value = 20
+            pb_load.Value = 20
             wc.DownloadFile("http://nonssl.dl.ministudios.ml/mini/elder/files/mini.exe", Application.StartupPath & "\mini.exe")
-            ProgressBar1.Value = 40
+            pb_load.Value = 40
             wc.DownloadFile("http://nonssl.dl.ministudios.ml/mini/elder/files/mini2.exe", Application.StartupPath & "\mini2.exe")
-            ProgressBar1.Value = 60
+            pb_load.Value = 60
             wc.DownloadFile("http://nonssl.dl.ministudios.ml/mini/elder/files/icon.ico", Application.StartupPath & "\icon.ico")
-            ProgressBar1.Value = 80
+            pb_load.Value = 80
         Catch ex As Exception
             MsgBox("Aktualizace se nezdařila! Chyba: " & ex.ToString, MsgBoxStyle.Critical, "Error")
             Application.Exit()
         End Try
-        ProgressBar1.Value = 100
+        pb_load.Value = 100
         Process.Start(Application.StartupPath & "\launcher.exe")
         Me.Close()
     End Sub
@@ -141,76 +144,82 @@ Public Class updater
             Snooze(1)
         Loop
         Me.Show()
-        ProgressBar1.Minimum = 0
-        ProgressBar1.Maximum = 100
+        pb_load.Minimum = 0
+        pb_load.Maximum = 100
         Try
-            ProgressBar1.Value = 1
+            pb_load.Value = 1
             wc.DownloadFile("http://nonssl.dl.ministudios.ml/mini/elder/beta/files/launcher.exe", Application.StartupPath & "\launcher.exe")
-            ProgressBar1.Value = 20
+            pb_load.Value = 20
             wc.DownloadFile("http://nonssl.dl.ministudios.ml/mini/elder/beta/files/mini.exe", Application.StartupPath & "\mini.exe")
-            ProgressBar1.Value = 40
+            pb_load.Value = 40
             wc.DownloadFile("http://nonssl.dl.ministudios.ml/mini/elder/beta/files/mini2.exe", Application.StartupPath & "\mini2.exe")
-            ProgressBar1.Value = 60
+            pb_load.Value = 60
             wc.DownloadFile("http://nonssl.dl.ministudios.ml/mini/elder/beta/files/icon.ico", Application.StartupPath & "\icon.ico")
-            ProgressBar1.Value = 80
+            pb_load.Value = 80
         Catch ex As Exception
             MsgBox("Aktualizace se nezdařila! Chyba: " & ex.ToString, MsgBoxStyle.Critical, "Error")
             Application.Exit()
         End Try
-        ProgressBar1.Value = 100
+        pb_load.Value = 100
         Process.Start(Application.StartupPath & "\launcher.exe")
         Me.Close()
     End Sub
 
     Public Sub mhd()
         Me.Show()
-        ProgressBar1.Minimum = 0
-        ProgressBar1.Maximum = 100
+        pb_load.Minimum = 0
+        pb_load.Maximum = 100
         Dim wc As WebClient = New WebClient()
         Try
-            ProgressBar1.Value = 1
+            pb_load.Value = 1
             wc.DownloadFile("https://dl.ministudios.ml/mhd/mhd.exe", Application.StartupPath & "/mhd.exe")
         Catch ex As Exception
             MsgBox("Aktualizace se nezdařila! Chyba: " & ex.ToString, MsgBoxStyle.Critical, "Error")
             Application.Exit()
         End Try
-        ProgressBar1.Value = 100
+        pb_load.Value = 100
         Process.Start(Application.StartupPath & "/mhd.exe")
         Me.Close()
     End Sub
 
     Public Sub fdc()
+        Dim exeloc As String
+        Dim mystr As String = My.Application.CommandLineArgs.Item(1)
+        Dim cut_at As String = "-e:"
+        Dim x As Integer = InStr(mystr, cut_at)
+        exeloc = mystr.Substring(x + cut_at.Length - 1)
+        la_text.Text = "Update in progress..."
         Me.Show()
-        ProgressBar1.Minimum = 0
-        ProgressBar1.Maximum = 100
+        pb_load.Minimum = 0
+        pb_load.Maximum = 100
         Dim wc As WebClient = New WebClient()
         Try
-            ProgressBar1.Value = 1
-            wc.DownloadFile("https://dl.ministudios.ml/fdc/fdc.exe", Application.StartupPath & "/fdc.exe")
+            pb_load.Value = 1
+            wc.DownloadFile("https://dl.ministudios.ml/fdc/fdc.exe", exeloc)
         Catch ex As Exception
-            MsgBox("Update was not intstalled! Error: " & ex.ToString, MsgBoxStyle.Critical, "Error")
+            MsgBox("Update failed! Error: " & ex.ToString, MsgBoxStyle.Critical, "Error")
             Application.Exit()
         End Try
-        ProgressBar1.Value = 100
-        Process.Start(Application.StartupPath & "/fdc.exe")
+        pb_load.Value = 100
+        Process.Start(exeloc)
         Me.Close()
     End Sub
 
     Public Sub antiezo()
         Me.Show()
-        ProgressBar1.Minimum = 0
-        ProgressBar1.Maximum = 100
+        pb_load.Minimum = 0
+        pb_load.Maximum = 100
         Dim wc As WebClient = New WebClient()
         Try
-            ProgressBar1.Value = 1
+            pb_load.Value = 1
             wc.DownloadFile("https://dl.ministudios.ml/antiezo/files/antiezo.exe", Application.StartupPath & "/antiezo.exe")
-            ProgressBar1.Value = 50
+            pb_load.Value = 50
             wc.DownloadFile("https://dl.ministudios.ml/antiezo/files/tov.bat", Application.StartupPath & "/tov.bat")
         Catch ex As Exception
             MsgBox("Aktualizace se nezdařila! Chyba: " & ex.ToString, MsgBoxStyle.Critical, "Error")
             Application.Exit()
         End Try
-        ProgressBar1.Value = 100
+        pb_load.Value = 100
         Process.Start(Application.StartupPath & "/antiezo.exe")
         Me.Close()
     End Sub
@@ -229,19 +238,42 @@ Public Class updater
             MsgBox("A path doesn't contain any exe file. BF2Updater will be downloaded to desktop, you can move it afterwards where you want, keep it on the desktop or rename it. We're sorry for this, it was a bug in version 1.0.3 and it was fixed. Thanks for your patience.", MsgBoxStyle.Information, "Bug!")
             exeloc = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) & "/BF2Updater.exe"
         End If
-        Label2.Text = "Update in progress"
+        la_text.Text = "Update in progress..."
         Me.Show()
-        ProgressBar1.Minimum = 0
-        ProgressBar1.Maximum = 100
+        pb_load.Minimum = 0
+        pb_load.Maximum = 100
         Dim wc As WebClient = New WebClient()
         Try
-            ProgressBar1.Value = 1
+            pb_load.Value = 1
             wc.DownloadFile("https://dl.ministudios.ml/bf2/updater/BF2Updater.exe", exeloc)
         Catch ex As Exception
             MsgBox("Update failed! Error: " & ex.ToString, MsgBoxStyle.Critical, "Error")
             Application.Exit()
         End Try
-        ProgressBar1.Value = 100
+        pb_load.Value = 100
+        Process.Start(exeloc)
+        Me.Close()
+    End Sub
+
+    Public Sub wur()
+        Dim exeloc As String
+            Dim mystr As String = My.Application.CommandLineArgs.Item(1)
+            Dim cut_at As String = "-e:"
+            Dim x As Integer = InStr(mystr, cut_at)
+            exeloc = mystr.Substring(x + cut_at.Length - 1)
+        la_text.Text = "Update in progress..."
+        Me.Show()
+        pb_load.Minimum = 0
+        pb_load.Maximum = 100
+        Dim wc As WebClient = New WebClient()
+        Try
+            pb_load.Value = 1
+            wc.DownloadFile("https://dl.ministudios.ml/wur/WUreset.exe", exeloc)
+        Catch ex As Exception
+            MsgBox("Update failed! Error: " & ex.ToString, MsgBoxStyle.Critical, "Error")
+            Application.Exit()
+        End Try
+        pb_load.Value = 100
         Process.Start(exeloc)
         Me.Close()
     End Sub
